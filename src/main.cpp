110ccd461a0fe33d742a441ff2afbaf8b5aa1e79
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2025-01-05 13:26:28 +0100

    main: add ncurses and display

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,13 @@
 #define TRI 3
 #define INIT(c){{{(c), (c), (c)}, {(c), (c), (c)}, {(c), (c), (c)}}} 
 
+#define CWHITE      1
+#define CYELLOW     2
+#define CGREEN      3
+#define CBLUE       4
+#define CRED        5
+#define CORANGE     6
+
 enum colors{
     WHITE,   // top
     YELLOW,  // bottom
@@ -18,16 +25,39 @@ enum colors{
     ORANGE,  // posterior
 };
 
-std::string toString(enum colors c){
+void toString(const enum colors c){
     switch(c){
-    //printf("\x1B[93mYTexting\033[0m\n");
-    case WHITE:     return "\x1B[1;37;37mW\033[0m";
-    case YELLOW:    return "\x1B[1;93;93mY\033[0m";
-    case GREEN:     return "\x1B[1;32;32mG\033[0m";
-    case BLUE:      return "\x1B[1;34;34mB\033[0m";
-    case RED:       return "\x1B[1;31;31mR\033[0m";
-    case ORANGE:    return "\x1B[1;95;95mO\033[0m";
-    default: return " ";
+    case WHITE:     
+        attron(COLOR_PAIR(CWHITE));
+        printw("W");
+        attroff(COLOR_PAIR(CWHITE));
+        break;
+    case YELLOW:
+        attron(COLOR_PAIR(CYELLOW));
+        printw("Y");
+        attroff(COLOR_PAIR(CYELLOW));
+        break;
+    case GREEN:
+        attron(COLOR_PAIR(CGREEN));
+        printw("G");
+        attroff(COLOR_PAIR(CGREEN));
+        break;
+    case BLUE:
+        attron(COLOR_PAIR(CBLUE));
+        printw("B");
+        attroff(COLOR_PAIR(CBLUE));
+        break;
+    case RED:
+        attron(COLOR_PAIR(CRED));
+        printw("R");
+        attroff(COLOR_PAIR(CRED));
+        break;
+    case ORANGE:
+        attron(COLOR_PAIR(CORANGE));
+        printw("O");
+        attroff(COLOR_PAIR(CORANGE));
+        break;
+    default: break;
     }
 }
 
@@ -47,50 +77,50 @@ Display the cube and additional informations
 void display(){
     // top
     for(int i = 0; i < TRI; i++){
-        std::cout << "       |";
+        printw("       |");
         for(int j = 0; j < TRI; j++){
-            std::cout << toString(T[i][j]) << (j==TRI-1?"|": " ");
+            (toString(T[i][j])); printw(j==TRI-1? "|": " ");
         }
-        std::cout << std::endl;
+        printw("\n");
     }
-    std::cout << std::endl;
+    printw("\n");
 
     // left front right posterior
     auto printLayer = [&](int layer){
-        std::cout << "|";
+        printw("|");
         for(int j = 0; j < TRI; j++){
-            std::cout << toString(L[layer][j]) << (j==TRI-1?"|": " ");
+            (toString(L[layer][j])); printw(j==TRI-1?"|": " ");
         }
-        std::cout << "|";
+        printw("|");
         for(int j = 0; j < TRI; j++){
-            std::cout << toString(F[layer][j]) << (j==TRI-1?"|": " ");
+            (toString(F[layer][j])); printw(j==TRI-1?"|": " ");
         }
-        std::cout << "|";
+        printw("|");
         for(int j = 0; j < TRI; j++){
-            std::cout << toString(R[layer][j]) << (j==TRI-1?"|": " ");
+            (toString(R[layer][j])); printw(j==TRI-1?"|": " ");
         }
-        std::cout << "|";
+        printw("|");
         for(int j = 0; j < TRI; j++){
-            std::cout << toString(P[layer][j]) << (j==TRI-1?"|": " ");
+            (toString(P[layer][j])); printw(j==TRI-1?"|": " ");
         }
-        std::cout << std::endl;
+        printw("\n");
     };
 
     for(int i = 0; i < TRI; i++){
         printLayer(i);
     }
-    std::cout << std::endl;
+    printw("\n");
 
     // bottom
     for(int i = 0; i < TRI; i++){
-        std::cout << "       |";
+        printw("       |");
         for(int j = 0; j < TRI; j++){
-            std::cout << toString(B[i][j]) << (j==TRI-1?"|": " ");
+            (toString(B[i][j])); printw(j==TRI-1?"|": " ");
         }
-        std::cout << std::endl;
+        printw("\n");
     }
 
-    std::cout << std::endl << std::endl;
+    printw("\n\n");
 }
 
 // Right Move
@@ -143,7 +173,7 @@ void MMove(){
 
 // Left Move
 void LMove(){
-    // save
+    // moving face
     enum colors posterior_2_0 = P[2][0];
     enum colors posterior_1_0 = P[1][0];
     enum colors posterior_0_0 = P[0][0];
@@ -163,29 +193,60 @@ void LMove(){
     T[2][0] = posterior_2_0;
     T[1][0] = posterior_1_0;
     T[0][0] = posterior_0_0;
+
+    // rotating left face
+    face LCopy;
+    LCopy = L;
+    L[0][2] = LCopy[0][0]; L[1][2] = LCopy[0][1]; L[2][2] = LCopy[0][2];
+    L[2][1] = LCopy[1][2];
+    L[2][0] = LCopy[2][2]; L[1][0] = LCopy[2][1]; L[0][0] = LCopy[2][0];
+    L[0][1] = LCopy[1][0]; 
 }
 
-int key_read(){
-    int c;
+void TMove(){
+    // moving face
+    enum colors posterior_2_0 = P[2][0];
+    enum colors posterior_1_0 = P[1][0];
+    enum colors posterior_0_0 = P[0][0];
+}
 
-    initscr();                 /* Start curses mode     */
-    c = getch();
-    endwin();                  /* End curses mode    */
+char key_read(){
+    return getch();
+}
 
-    std::cout << "Char: " << c << std::endl;
+void move(char key){
+    switch(key){
+    case 'j': LMove(); break;
+    case 'k': MMove(); break;
+    case 'l': RMove(); break;
 
-    return c;
+
+    default: break;
+    }
 }
 
 int main(){
+    initscr();                 /* Start curses mode     */
+    start_color();
+
+    // colors init
+    init_pair(CWHITE, COLOR_BLACK, COLOR_WHITE);
+    init_pair(CYELLOW, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(CGREEN, COLOR_BLACK, COLOR_GREEN);
+    init_pair(CBLUE, COLOR_BLACK, COLOR_BLUE);
+    init_pair(CRED, COLOR_BLACK, COLOR_RED);
+    init_pair(CORANGE, COLOR_BLACK, COLOR_MAGENTA);
+
+    // game loop
     while(true){
+        clear();
+
         display();
-        int key = key_read(); 
+        refresh();
+
+        char key = key_read(); 
+        move(key);
     }
 
-    display();
-    MMove();
-    display();
-    LMove();
-    display();
+    endwin();                  /* End curses mode    */
 }
